1083d83360de672a0f3332e0fd826c5341ee987a
Raniaelmorabet <122817081+Raniaelmorabet@users.noreply.github.com>
2024-07-22 12:02:07 +0100

    orderDay

--- SchoolManagementSystem.Domain/Entities/DayOrder.cs
+++ SchoolManagementSystem.Domain/Entities/DayOrder.cs
@@ -5,5 +5,6 @@ namespace SchoolManagementSystem.Domain.Entities
     public class DayOrder : Base
     {
         public string? DayOrderTitle { get; set; }
+        public string? FilePath { get; set; }
     }
 }
